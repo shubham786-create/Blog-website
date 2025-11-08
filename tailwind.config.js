329bda9dd9f132b6409dc19310642962b8350322
Shubham Pandey <pandeyshubham0475@gmail.com>
2025-11-08 21:15:22 +0530

    Initialize repo

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+// tailwind.config.js
+module.exports = {
+  content: [
+    "./views/**/*.ejs", // Update this path to match your template engine and structure
+    "./public/javascripts/**/*.js"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
